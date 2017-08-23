8d362b7d3d22e711fb8e4c304924ed0a4b48bf53
Matt Bacher <mattjb1991@sbcglobal.net>
2017-08-23 18:31:48 -0400

    final commit

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
-
-	var topics = ["chicken parm", "steak", "cheeseburger", "tacos", "nachos", "grilled cheese", "cheesesteak", "carnitas", "mac and cheese", "salmon fillets", "mashed potatoes"];
+	//
+	var topics = ["chicken parm", "steak", "ice cream", "tacos", "nachos", "grilled cheese", "cheesesteak", "carnitas", "mac and cheese", "salmon", "mashed potatoes"];
 
 	function createButtons() {
 		
@@ -11,20 +11,41 @@ $(document).ready(function(){
 
 			var button = $("<button>");
 
-			button.addClass(".buttonDesign");
+			button.addClass("buttonDesign");
 			button.attr("data-name", topics[i]);
 			button.text(topics[i]);
 			$("#buttonDisplay").append(button);
 
 		}
 	};
+
+	//Calling the functon to creat buttons
 	createButtons();
 
-	$("button").on("click", function() {
+	$("#add-food").on("click", function(event) {
+
+		event.preventDefault();
+
+		var food = $("#food-input").val().trim();
+
+		topics.push(food);
+
+		createButtons();
+	});
+
+
+
+	//	Doing the ajax search when the button is clicked on and assigning the attributes neccessary
+	//	to do the animation process
+	function ajaxCall () {
+
+		$("#gifDisplay").empty();
 
 		var term = $(this).attr("data-name");
-		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
 		        term + "&api_key=dc6zaTOxFJmzC&limit=10";
+		       
+		        
 
 		$.ajax({
 			url: queryURL,
@@ -32,28 +53,71 @@ $(document).ready(function(){
 		})
 		.done(function(response) {
 
-			console.log(response.data);
+			var result = response.data;
+
+				for (var i = 0; i < result.length; i++) {
+
+					var gifRating = result[i].rating;
+
+					var ratingText = $("<p>").text("Rating: " + gifRating);
+
+					var GIFimage = $("<img>");
+					
+					GIFimage.attr("src", result[i].images.fixed_height_still.url);
+					GIFimage.attr("data-still", result[i].images.fixed_height_still.url);
+					GIFimage.attr("data-animate", result[i].images.fixed_height.url);
+					GIFimage.attr("data-state", "still");
+					GIFimage.addClass("clickableImage");
+					$("#gifDisplay").append(GIFimage);
+					$("#gifDisplay").append(ratingText);
+				};
+
+
+			$("img").on("click", function() {
+		        
+		        var state = $(this).attr("data-state");
+
+		        if (state === "still") {
+		          $(this).attr("src", $(this).attr("data-animate"))
+		          $(this).attr("data-state", "animate")
+		        }
+
+		        else {
+		          $(this).attr("src", $(this).attr("data-still"))
+		          $(this).attr("data-state", "still")
+		        }
+
+			});
+		});	
+	};
+
+	$(document).on("click", ".buttonDesign", ajaxCall);
+
+
+	// tried to make a random food gif appear when the random button was clicked but ran into isses
+
+	$("#random").on("click", function(event) {
+
+
+		var queryRandomURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=junk food";
+
+		$.ajax({
+
+			url: queryRandomURL,
+			method: "GET"	
+		})
+		.done(function(response){
+
+		
+
+			var randomFood = $("<img>");
+
+			randomFood.attr("src", response.data.images.fixed_height.url);
+			$("#gifDisplay").prepend(randomFood);
+			
 		});
-
-	
-
 
 
 	});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
+
